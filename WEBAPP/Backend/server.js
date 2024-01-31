4fbf87cedd55b8d38edef1d95bddcad278acd018
Rakesh Kanneeswaran <rakeshkanneeswaran@Rakeshs-MacBook-Pro.local>
2024-01-31 13:44:22 +0530

    added stable diffusion model

---
+++ WEBAPP/Backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`);
+});
